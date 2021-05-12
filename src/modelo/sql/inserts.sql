6de62c08047f86ff3a89823e379694b75175d64a
lazcozlaz <lazcozlaz@gmail.com>
2021-05-12 13:14:34 +0200

    inserts

--- src/modelo/sql/inserts.sql
+++ src/modelo/sql/inserts.sql
@@ -1,5 +1,20 @@
 insert into figuras(fig_tipo, fig_nombre, fig_lado)  values(1,"cua1", 10)
-insert into figuras(fig_tipo, fig_nombre, fig_radio)  values(1,"cir1", 10)
+insert into figuras(fig_tipo, fig_nombre, fig_radio)  values(2,"cir1", 10)
 insert into figuras(fig_tipo, fig_nombre,fig_base, fig_altura )  values(4,"trianuglo1", 10, 8.4)
 insert into figuras(fig_tipo, fig_nombre,fig_base, fig_altura )  values(3,"rect1", 12.3, 8.4)
 insert into figuras(fig_tipo, fig_nombre,fig_lado, fig_numlados,fig_apotema )  values(5,"polig1", 10,6, 6.2)
+
+update figuras
+set FIG_NOMBRE ='cuadrado' where FIG_TIPO = 1
+
+update figuras
+set FIG_NOMBRE ='circulo' where FIG_TIPO = 2
+
+update figuras
+set FIG_NOMBRE ='triangulo' where FIG_TIPO = 4
+
+update figuras
+set FIG_NOMBRE ='rectangulo' where FIG_TIPO = 3
+
+update figuras
+set FIG_NOMBRE ='poligono' where FIG_TIPO = 5
